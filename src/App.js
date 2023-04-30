c7dc47dab48e488bcf1023f0ef3b3204700c386c
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-29 23:16:46 -0400

    added styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
 import Footer from './components/Footer';
 import Grid from './components/Grid';
 import Header from './components/Header';
 import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
 
 function App() {
   return (
--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -6,7 +6,7 @@ export default function CardGrid({food}) {
                 <div key={food.id}>
                     <img src={food.image} height={280} width={280}/>
                     <h2>{food.name}</h2>
-                    <p>Tiempo de preparacion: {food.time} min</p>
+                    <p>Preparation time: {food.time} min</p>
                     {food.ingredients.map((ingredient) => <li>{ingredient}</li>)}
                 </div>
                 
