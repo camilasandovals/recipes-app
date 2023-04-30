ef03de2e6dd5ec51895d912a8ee385c82f4aac62
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-30 00:00:46 -0400

    added ingredients

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,21 +1,23 @@
 import { useState } from "react"
 
-export default function CardGrid({food}) {
-    const [showIngredients, setShowIngredients] = useState(false)
+export default function CardGrid({ food }) {
+  const [showIngredients, setShowIngredients] = useState(false)
 
-    return(
-            <>  
-            
-                <div key={food.id} >
-                    <img src={food.image} height={280} width={280}/>
-                    <h2>{food.name}</h2>
-                    <p>Preparation time: {food.time} min</p>
-                    <button onClick = {() => setShowIngredients(!showIngredients)}>Ingredients</button>
-                    <p>{showIngredients ? food.ingredients : ' '}
-                    </p>
-                </div>
-                
-
-        </>
-    )
+  return (
+    <>
+      <div key={food.id}>
+        <img src={food.image} height={280} width={280} />
+        <h2>{food.name}</h2>
+        <p>Preparation time: {food.time} min</p>
+        <button onClick={() => setShowIngredients(!showIngredients)}>Ingredients</button>
+        {showIngredients && (
+          <ul>
+            {food.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
+  )
 }
