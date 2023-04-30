e19b100540855abec4273c5665880be611de5144
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-29 23:50:12 -0400

    added button

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react"
 
 export default function CardGrid({food}) {
+    const [showIngredients, setShowIngredients] = useState(false)
+
     return(
             <>  
             
-                <div key={food.id}>
+                <div key={food.id} >
                     <img src={food.image} height={280} width={280}/>
                     <h2>{food.name}</h2>
                     <p>Preparation time: {food.time} min</p>
-                    {food.ingredients.map((ingredient) => <li>{ingredient}</li>)}
+                    <button onClick = {() => setShowIngredients(!showIngredients)}>Ingredients</button>
+                    <p>{showIngredients ? food.ingredients : ' '}
+                    </p>
                 </div>
                 
 
